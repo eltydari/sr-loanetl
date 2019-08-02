98bb89d124f2dac55d5bb41d33c2547710316629
george-ge <2453869+george-ge@users.noreply.github.com>
2019-08-01 23:22:51 -0400

    Consolidated all io operations

--- features/steps/io.py
+++ features/steps/io.py
@@ -1,6 +1,7 @@
-# -- FILE: features/steps/ops_file.py
-import os.path
+# -- FILE: features/steps/io.py
 import json
+from   lib.assertions import assertItemsEqual
+import os.path
 import tempfile as tmp
 
 @given(u"we have a folder")
@@ -16,3 +17,15 @@ def given_named_file_containing(context, fileName):
 @given(u"a configuration map with the following representation")
 def given_map_containing(context):
     context.cfg = json.loads(context.text)
+
+@then(u"I will see the following table")
+def then_compare_tables(context):
+    expected = context.table
+    assert expected, "Ensure table is provided."
+    observed = context.result
+    assertItemsEqual([str(x) for x in observed.columns], 
+                        expected.headings, "Headings do not match.")
+    assert len(expected.rows) == len(observed), "Number of rows does not match."
+    for i in range(len(expected.rows)):
+        assertItemsEqual([str(x) for x in observed.loc[i].tolist()], 
+                                list(expected.rows[i]), "Rows do not match in content.")
